2ca15711eafa4c498904fe07c78056540fec5711
akash thakur <akash@akashs-mbp.home>
2020-03-18 21:39:49 -0400

    check number of arguments, and length

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
 // Parse inline parameters using slice function.
 const args = process.argv.slice(2);
+
+// Check if there are more or less than 2 input parameters.
+
+if (args.length != 2) {
+	console.log("Error : Number if arguments are incorrect");
+} else {
+	const str1 = args[0];
+	const str2 = args[1];
+	return Mapping(str1, str2);
+}
+
+
+if( str1.length > str2.length) {
+	console.log("Fatta")
+} 
